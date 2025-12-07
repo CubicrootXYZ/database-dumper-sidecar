37f6cd911739ca8548884a02bd58fa1a34d294c8
cubicroot <github@cubicroot.xyz>
2025-12-07 16:46:37 +0100

    fix: skip ssl

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ do
 
     # Start new backup
     echo "Dumping database now"
-    mysqldump -A -h $DB_HOST -p${DB_PASSWORD} -u $DB_USER -P $DB_PORT > /backup/database_dump.sql
+    mysqldump --skip-ssl -A -h $DB_HOST -p${DB_PASSWORD} -u $DB_USER -P $DB_PORT > /backup/database_dump.sql
 
     # Sleep
     echo "Sleeping for ${SLEEP_SECONDS} now"
